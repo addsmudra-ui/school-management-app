bc4c5de3a6b85ce6ab9777056785380008d1d691
Mudra Communications <addsmudra@gmail.com>
2026-03-08 17:36:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5734666585-ddcd5",
+  "appId": "1:626618470267:web:5c5880650e987c0af737de",
+  "apiKey": "AIzaSyA3oE_NBOBMXtk9xjQJGFzTRTSgIBOwvy8",
+  "authDomain": "studio-5734666585-ddcd5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "626618470267"
+};
